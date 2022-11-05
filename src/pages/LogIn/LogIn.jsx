cc3a32093ef2a645a3683c6460eca3dedbb24bcf
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-11-05 10:34:25 +0600

    Login form basic setup complete

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import login from "../../assets/login.svg";
+import Container from "../../layout/Container/Container";
 
 const LogIn = () => {
-  return <div>LogIn</div>;
+  return (
+    <Container>
+      <div className="h-screen flex items-center justify-center">
+        <div className=" w-full mx-auto  bg-slate-50  rounded-md shadow-sm pt-5">
+          <h2 className="text-3xl font-bold text-center ">Log In</h2>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 px-10 py-10 ">
+            <div>
+              <form>
+                <div className="mb-6">
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name@flowbite.com"
+                    required=""
+                  />
+                </div>
+                <div className="mb-6">
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    Your password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+            <div>
+              <img src={login} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default LogIn;
